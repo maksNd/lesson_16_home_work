21aadcb17dfeee36e36f8cb08a0f10c927cecd49
mkNd <mknord1565@gmail.com>
2022-08-27 13:40:32 +0300

    improvements

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from db.model import app
+from db.fill_DB import fill_DB
 from bp_users.bp_users import bp_users
 from bp_orders.bp_orders import bp_orders
 from bp_offers.bp_offers import bp_offers
+
+fill_DB()
 
 app.register_blueprint(bp_users, url_prefix='/users/')
 app.register_blueprint(bp_orders, url_prefix='/orders/')
--- db/fill_DB.py
+++ db/fill_DB.py
@@ -1,8 +1,5 @@
 import json
-from model import db, User, Offer, Order
-
-db.drop_all()
-db.create_all()
+from db.model import db, User, Offer, Order
 
 
 def get_data_from_json(path):
@@ -10,15 +7,22 @@ def get_data_from_json(path):
         return json.load(file)
 
 
-users_from_json = get_data_from_json('db/json_files/users.json')
-orders_from_json = get_data_from_json('db/json_files/orders.json')
-offers_from_json = get_data_from_json('db/json_files/offers.json')
+def fill_DB():
+    db.drop_all()
+    db.create_all()
 
-users = [User(**row) for row in users_from_json]
-orders = [Order(**row) for row in orders_from_json]
-offers = [Offer(**row) for row in offers_from_json]
+    users_from_json = get_data_from_json('db/json_files/users.json')
+    orders_from_json = get_data_from_json('db/json_files/orders.json')
+    offers_from_json = get_data_from_json('db/json_files/offers.json')
 
-db.session.add_all(users)
-db.session.add_all(orders)
-db.session.add_all(offers)
-db.session.commit()
+    users = [User(**row) for row in users_from_json]
+    orders = [Order(**row) for row in orders_from_json]
+    offers = [Offer(**row) for row in offers_from_json]
+
+    db.session.add_all(users)
+    db.session.add_all(orders)
+    db.session.add_all(offers)
+    db.session.commit()
+
+
+# fill_DB()
